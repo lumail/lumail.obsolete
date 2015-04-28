64d99bce99dc42f2a1a6f36aeb8876382b8df942
Raphaël Rigo <devel-git@syscall.eu>
2015-04-28 15:51:19 +0200

    Fix typo in attachment names, should fix bug #241

--- src/message.cc
+++ src/message.cc
@@ -1412,7 +1412,7 @@ bool CMessage::parse_attachments()
             if ( ( view_inline == true ) ||
                  ( view_inline == false && ( is_inline == false ) ) )
             {
-                CAttachment *foo = new CAttachment( tmp, (void *)adata,(size_t ) len );
+                CAttachment *foo = new CAttachment( aname, (void *)adata,(size_t ) len );
                 m_attachments.push_back(foo);
             }
         }
